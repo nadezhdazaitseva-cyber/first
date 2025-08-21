13fca43cdd87bf0c16d5d3d5603c28d749ad967d
Hope Luzina <mrs.foreveralone@gmail.com>
2025-08-21 10:59:44 +0300

    create the apportunity to input  user db params for connect

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def main():
     rooms = "https://raw.githubusercontent.com/nadezhdazaitseva-cyber/first/refs/heads/main/rooms.json"
     students = "https://raw.githubusercontent.com/nadezhdazaitseva-cyber/first/refs/heads/main/students.json"
 
+    # Default database connection parameters
     db_params = {
         'host': 'localhost',
         'database': 'postgres',
@@ -18,8 +19,26 @@ def main():
         'password': '123',
         'port': '5432'
     }
+    print('''We are going to work with the PostgreSQL database.
+Do you want to use the default connection parameters [y - YES, n - NO]?
+If not[n], you can enter your own parameters.''')
+    
+    input_db_params = input().strip().lower()
+    
+    if input_db_params == 'y':
+        print("Using default connection parameters.")
+        db_manager = DatabaseManager(db_params)
+    elif input_db_params == 'n':
+        db_params['host'] = input("Input host:").strip().lower()
+        db_params['database'] = input("Input database name:").strip().lower()
+        db_params['user'] = input("Input user name:").strip().lower()
+        db_params['password'] = input("Input password:").strip() 
+        db_params['port'] = input("Input port:").strip().lower()
+        db_manager = DatabaseManager(db_params)
+    else:
+        print("Incorrect input. Using default connection parameters.")
+        db_manager = DatabaseManager(db_params)
 
-    db_manager = DatabaseManager(db_params)
     try:
         db_manager.connect()
         data_handler = DataHandler(rooms, students)
@@ -53,23 +72,23 @@ def main():
         if choice in all_queries:
             query_key = choice
             selected_queries[all_queries[query_key]['name']] = all_queries[query_key]['query']
+            results = data_processor.run_queries(selected_queries)
+
+            # Handle output format
+            format_choice = input("Enter output format (j - json, x - xml или p - plain): ")
+            if format_choice.lower() == 'j':
+                with open('results.json', 'w') as f:
+                    json.dump(results, f, indent=2)
+                    print("Results saved to results.json")
+            elif format_choice.lower() == 'x':
+                xml_generator.save_xml(results)
+            else:
+                for query_name, result in results.items():
+                    print(f"\n--- Results for: {query_name} ---")
+                    [print(*el) for el in result]
         else:
             print("Incorrect number. No request will be executed.")
 
-        results = data_processor.run_queries(selected_queries)
-
-        # Handle output format
-        format_choice = input("Enter output format (j - json, x - xml или p - plain): ")
-        if format_choice.lower() == 'j':
-            with open('results.json', 'w') as f:
-                json.dump(results, f, indent=2)
-            print("Results saved to results.json")
-        elif format_choice.lower() == 'x':
-            xml_generator.save_xml(results)
-        else:
-            for query_name, result in results.items():
-                print(f"\n--- Results for: {query_name} ---")
-                [print(*el) for el in result]
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
